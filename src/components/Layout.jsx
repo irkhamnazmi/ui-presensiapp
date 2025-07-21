3813718bea9864f97aea3811f8d8d6addc5523ed
irkhamnazmi <irkham.nazmi@gmail.com>
2025-07-21 13:54:52 +0700

    refactor: remove react-helmet

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -13,45 +13,15 @@ export default function Layout({ children }) {
     !location.startsWith("/login") && !location.startsWith("/404");
 
   return (
-    <>
-      <Helmet>
-        <title>PresensiApp | Aplikasi Kehadiran</title>
-        <meta
-          name="description"
-          content="PresensiApp adalah aplikasi pengelola presensi sederhana."
-        />
-        <meta property="og:title" content="PresensiApp | Aplikasi Kehadiran" />
-        <meta
-          property="og:description"
-          content="PresensiApp adalah aplikasi pengelola presensi sederhana."
-        />
-        <meta
-          property="og:image"
-          content="https://presensiapp.netlify.app/images/presensiapp.png"
-        />
-        <meta property="og:url" content="https://presensiapp.netlify.app/" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="PresensiApp | Aplikasi Kehadiran" />
-        <meta
-          name="twitter:description"
-          content="PresensiApp adalah aplikasi pengelola presensi sederhana."
-        />
-        <meta
-          name="twitter:image"
-          content="https://presensiapp.netlify.app/images/presensiapp.png"
-        />
-      </Helmet>
+    <div className="min-h-screen bg-white px-16">
+      {/* Navbar */}
+      {shouldShowNav && <Navbar />}
 
-      <div className="min-h-screen bg-white px-16">
-        {/* Navbar */}
-        {shouldShowNav && <Navbar />}
+      {/* Main */}
+      <main className="border-slate-500">{children}</main>
 
-        {/* Main */}
-        <main className="border-slate-500">{children}</main>
-
-        {/* Bottom Navigation */}
-        {shouldShowBottomNav && <BottomNav />}
-      </div>
-    </>
+      {/* Bottom Navigation */}
+      {shouldShowBottomNav && <BottomNav />}
+    </div>
   );
 }
